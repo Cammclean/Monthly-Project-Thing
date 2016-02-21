b867622f2c45c7b6774c6e11689f9f088b4c8ba4
Cammclean <cameron.mclean@selu.edu>
2016-02-21 15:01:16 -0600

    Scene Management added
    -Organized scripts and scenes into folders
    -Added scene management functions into the EnterAreaScript script

---
+++ Project Thing/Assets/Scripts/EnterAreaScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnterAreaScript : MonoBehaviour {
+    public GameObject player;
+    public int nextLevel;
+    public int currLevel;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject == player)
+        {
+            SceneManager.LoadScene(nextLevel, LoadSceneMode.Additive);
+
+            GameObject camera = Camera.main.gameObject;
+
+            SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneAt(nextLevel));
+            SceneManager.MoveGameObjectToScene(camera, SceneManager.GetSceneAt(nextLevel));
+
+            StartCoroutine(UnloadScene());
+        }
+
+    }
+
+    IEnumerator UnloadScene()
+    {
+        yield return new WaitForSeconds(0.1f);
+        SceneManager.UnloadScene(currLevel);
+
+        StartCoroutine(ClearScene());
+    }
+
+    IEnumerator ClearScene()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Resources.UnloadUnusedAssets();
+    }
+}
